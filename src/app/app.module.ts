684f8008ec08ca87ec6fe08e8ca44a564b57a3b1
lethanhdat9521 <72382721+lethanhdat9521@users.noreply.github.com>
2023-04-11 16:14:29 +0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoadingModalComponent } from './components/loading-modal/loading-modal.component';
+import { FailModalComponent } from './components/fail-modal/fail-modal.component';
+import { SuccessModalComponent } from './components/success-modal/success-modal.component';
+import { AdminIndexComponent } from './components/admin-index/admin-index.component';
+import { RouterOutlet } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { TopNavComponent } from './components/top-nav/top-nav.component';
+import { LeftNavComponent } from './components/left-nav/left-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoadingModalComponent,
+    FailModalComponent,
+    SuccessModalComponent,
+    AdminIndexComponent,
+    TopNavComponent,
+    LeftNavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule, RouterOutlet, AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/admin-index/admin-index.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+//@Component({
+//  selector: 'app-admin-index',
+//  templateUrl: './admin-index.component.html',
+//  styleUrls: ['./admin-index.component.css']
+//})
+@Component({
+  selector: 'app-admin-index',
+  templateUrl: './index.component.html',
+  styleUrls: ['./admin-index.component.css']
+})
+
+export class AdminIndexComponent {
+
+}
